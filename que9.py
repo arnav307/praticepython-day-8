d0bf7f82c7f98024eddbe208b68fd1bb9fae9b6b
Arnavghimire <ghimirearnav738@gmail.com>
2025-03-23 21:56:24 +0545

    add it

---
+++ que9.py
@@ -0,0 +1,6 @@
+list_of_string=["arnav","ghimire","nepal","apple","swornim","banana"]
+new_list=[]
+for i in list_of_string:
+    if 'a' in i:
+        new_list.append(i)
+print("The new list contain letter A: ",new_list)
